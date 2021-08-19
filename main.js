83eb83771155db21e5e3480244b7dcb01f604caf
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-08-19 18:36:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,65 @@
 video = "";
 status = "";
+objects = [];
+object = "";
 
-function preload()
-{
+function preload() {
 
 }
 
-function setup()
-{
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
-
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
 }
 
-function modelLoaded()
-{
+function modelLoaded() {
     console.log("Model Is Initialized");
     status = true;
 }
 
-function draw()
-{
+function draw() {
     image(video, 0, 0, 600, 500);
+
+    if (status != "") {
+
+        objectDetector.detect(video, gotresult);
+
+        for (i = 0; i < objects.length; i++) {
+            
+            if (object == objects[i].label) {
+                text(objects[i].label + " " + percentage + "%", objects[i].x + 5, objects[i].y + 10);
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+                document.getElementById("status").innerHTML = "Object Found";
+                video.stop();
+                objectDetector.detect(gotresult);
+
+                var synth = window.speechSynthesis;
+                speak_data_1 = objects[i].label + "Found";
+                var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+                synth.speak(utterThis);
+            }
+            else {
+                document.getElementById("status").innerHTML = "Object Not Found";
+            }
+        }
+    }
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        object = results;
+    }
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    object = document.getElementById("object").value;
 }
